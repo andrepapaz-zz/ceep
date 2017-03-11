2c0c5702fe3d5be8f4f84e8dc82044c80ad8070c
andrepapaz <andre.papazoglu@gmail.com>
2017-03-11 00:04:50 -0300

    até a aula 5, com cache storage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,20 +1,73 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = []
+    let cartoes = pegaCartoesUsuario()
+    
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
-
+    render()
     Filtro.on("filtrado", render)
 
-    function adiciona(cartao){
-        cartoes.push(cartao)
-        cartao.on("mudanca.**", render)
+    function preparaCartao(cartao){
+        const urlImagens = Cartao.pegaImagens(cartao)
+
+        urlImagens.forEach(url => {
+            fetch(url).then(resposta => {
+                caches.open("ceep-imagens").then(cache => {
+                    cache.put(url, resposta)
+                })
+            })
+        })
+
+        cartao.on("mudanca.**", salvaCartoes)
         cartao.on("remocao", ()=>{
             cartoes = cartoes.slice(0)
             cartoes.splice(cartoes.indexOf(cartao),1)
+            salvaCartoes()
             render()
         })
+    }
+
+    function pegaCartoesUsuario(){
+        let cartoesLocal = JSON.parse(localStorage.getItem(usuario))
+
+        if(cartoesLocal) {
+            return cartoesLocal.map(cartaoLocal => {
+                let cartao = new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo)
+                preparaCartao(cartao)
+                return cartao
+            })
+        } else {
+            return []
+        }
+    }
+
+    function salvaCartoes(){
+        localStorage.setItem(usuario, JSON.stringify(
+            cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+        ))
+    }
+
+    login.on("login", () => {
+        cartoes = pegaCartoesUsuario()
         render()
-        return true
+    })
+
+    login.on("logout", () => {
+        cartoes = []
+        render()
+    })
+
+    function adiciona(cartao){
+        if (logado) {
+            cartoes.push(cartao)
+            salvaCartoes()
+            cartao.on("mudanca.**", render)
+            preparaCartao(cartao)
+
+            render()
+            return true
+        } else {
+            alert("Você não está logado")
+        }
     }
 
     return Object.seal({
